aaabe9e9217b82163b0f75f4777356c5d556998b
callmejogi <kutafin01@gmail.com>
2021-10-05 20:42:57 +0300

    finish task

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
-let user = {
-    name: "John",
-    surname: "Smith",
-    name: "Pete",
-};
+//task#1
+let user = {};
+
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
 
 console.log(user);
 
 delete user.name;
+
+//task#2
+const user1 = {
+    name:"John"
+};
+
+user1.name = "Pete";
+console.log(user1)
+
+//task#3
+let salaries = {
+    John:100,
+    Ann:160,
+    Pete:130
+};
+
+let sum = salaries.John + salaries.Ann + salaries.Pete;
+console.log(`Sum of salaries:${sum}`)
